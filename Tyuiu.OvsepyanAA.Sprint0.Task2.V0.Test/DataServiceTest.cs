99492f53b94df8cb7a8e1cc7ef76e42616de8da4
aramovsepyan2006@outlook.com <aramovsepyan2006@outlook.com>
2024-09-21 23:13:55 +0500

    Task2

---
+++ Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckMessangeValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Арам";
+            var res = DataService.GetMessage(name);
+
+            //Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Арам", res);
+        }
+    }
+}
---
+++ Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.OvsepyanAA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06402abe-088a-43cf-ac9b-741c0ed22fad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
